70d77ee41ea3499891f837b069b03211c3b93493
woowahan <woowahan@woowahan-MacBookPro-55.local>
2022-07-12 16:37:30 +0900

    [FEAT] [Login] 인증 코드 발급

--- app/src/main/java/com/example/android_repo_04/view/LoginActivity.kt
+++ app/src/main/java/com/example/android_repo_04/view/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.android_repo_04.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.net.toUri
+import com.example.android_repo_04.BuildConfig
 import com.example.android_repo_04.R
 import com.example.android_repo_04.databinding.ActivityLoginBinding
 import com.example.android_repo_04.databinding.ActivityMainBinding
@@ -11,8 +14,8 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
-    val btnLoginClickListener: (View) -> Unit = {
-
+    private val btnLoginClickListener: (View) -> Unit = {
+        startActivity(Intent(Intent.ACTION_VIEW, "${BuildConfig.LOGIN_URL}${BuildConfig.CLIENT_ID}".toUri()))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +24,6 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
         setOnClickListeners()
     }
-
 
     private fun setOnClickListeners(){
         binding.btnLoginLogin.setOnClickListener (btnLoginClickListener)
