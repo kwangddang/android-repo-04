232723fffc637d927623e9c5f9c96ee09c0c4250
KwangHyun Choi <camp_kwangddang12@woowafriends.com>
2022-07-12 15:26:51 +0900

    [FEAT] [Login] 버튼 클릭 리스너

--- app/src/main/java/com/example/android_repo_04/view/LoginActivity.kt
+++ app/src/main/java/com/example/android_repo_04/view/LoginActivity.kt
@@ -2,11 +2,28 @@ package com.example.android_repo_04.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.android_repo_04.R
+import com.example.android_repo_04.databinding.ActivityLoginBinding
+import com.example.android_repo_04.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    val btnLoginClickListener: (View) -> Unit = {
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setOnClickListeners()
+    }
+
+
+    private fun setOnClickListeners(){
+        binding.btnLoginLogin.setOnClickListener (btnLoginClickListener)
     }
 }
