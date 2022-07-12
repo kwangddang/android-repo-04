7cacc03ca620e672ca9884b1f7b28c68a40c5146
KwangHyun Choi <camp_kwangddang12@woowafriends.com>
2022-07-12 11:17:45 +0900

    [FEAT] [Login] Login Activity 생성

---
+++ app/src/main/java/com/example/android_repo_04/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android_repo_04.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android_repo_04.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
