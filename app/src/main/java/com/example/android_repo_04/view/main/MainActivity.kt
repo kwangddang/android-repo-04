eb8144e9d328359a3a455d492251b3e175d4094d
KwangHyun Choi <camp_kwangddang12@woowafriends.com>
2022-07-13 12:56:58 +0900

    [FEAT] [Main] 버튼 클릭시 색 변경

--- app/src/main/java/com/example/android_repo_04/view/main/MainActivity.kt
+++ app/src/main/java/com/example/android_repo_04/view/main/MainActivity.kt
@@ -2,11 +2,43 @@ package com.example.android_repo_04.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
 import com.example.android_repo_04.R
+import com.example.android_repo_04.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val btnIssueClickListener: (View) -> Unit = {
+        selectIssue()
+    }
+
+    private val btnNotificationClickListener: (View) -> Unit = {
+        selectNotification()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        selectIssue()
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners(){
+        binding.btnMainIssue.setOnClickListener(btnIssueClickListener)
+        binding.btnMainNotification.setOnClickListener(btnNotificationClickListener)
+    }
+
+    private fun selectIssue(){
+        binding.btnMainIssue.setBackgroundResource(R.drawable.btn_round_selected)
+        binding.btnMainNotification.setBackgroundResource(R.drawable.btn_round_unselected)
+    }
+
+    private fun selectNotification(){
+        binding.btnMainIssue.setBackgroundResource(R.drawable.btn_round_unselected)
+        binding.btnMainNotification.setBackgroundResource(R.drawable.btn_round_selected)
     }
 }
