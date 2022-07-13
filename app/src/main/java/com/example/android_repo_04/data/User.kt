5ae9cbd25cecc1c3c21ac340694af51c497ee76e
rosf73 <alswlzhsclq@naver.com>
2022-07-13 22:58:02 +0900

    [REFACTORY] [NOTIFICATIONS] 테스트 코드 삭제

--- app/src/main/java/com/example/android_repo_04/data/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.android_repo_04.data
-
-import com.google.gson.annotations.SerializedName
-
-data class User(
-    @SerializedName("login") val login: String,
-    @SerializedName("avatar_url") val avatarUrl: String
-)
