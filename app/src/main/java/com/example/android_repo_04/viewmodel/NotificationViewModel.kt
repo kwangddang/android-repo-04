f2cdbb673c55afe665d0f32a4c21e925c6be0bf4
rosf73 <alswlzhsclq@naver.com>
2022-07-14 12:30:17 +0900

    [FEAT] [Notifications] Notifications ViewModel 작성

--- app/src/main/java/com/example/android_repo_04/viewmodel/NotificationViewModel.kt
+++ app/src/main/java/com/example/android_repo_04/viewmodel/NotificationViewModel.kt
@@ -1,4 +1,38 @@
 package com.example.android_repo_04.viewmodel
 
-class NotificationViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.android_repo_04.api.GitHubApiRepository
+import com.example.android_repo_04.data.dto.notificaiton.Notification
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NotificationViewModel(private val gitHubApiRepository: GitHubApiRepository): ViewModel() {
+
+    private val _notification = MutableLiveData<List<Notification>>()
+    val notification: LiveData<List<Notification>> get() = _notification
+
+    fun requestNotifications(token: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            gitHubApiRepository.requestNotifications(token) {
+                if (it.isSuccessful) {
+                    _notification.postValue(it.body())
+                } else {
+
+                }
+            }
+        }
+    }
+}
+
+class NotificationViewModelFactory(private val gitHubApiRepository: GitHubApiRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(NotificationViewModel::class.java)) {
+            return NotificationViewModel(gitHubApiRepository) as T
+        }
+        throw IllegalAccessException()
+    }
 }
