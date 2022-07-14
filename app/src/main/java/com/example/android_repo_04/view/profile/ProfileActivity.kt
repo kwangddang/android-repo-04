a33c32d753eb2a2dcf959528445f981ecc7ab218
KwangHyun Choi <camp_kwangddang12@woowafriends.com>
2022-07-14 11:26:13 +0900

    [FEAT] [Profile] init ProfileViewModel

--- app/src/main/java/com/example/android_repo_04/view/profile/ProfileActivity.kt
+++ app/src/main/java/com/example/android_repo_04/view/profile/ProfileActivity.kt
@@ -2,11 +2,29 @@ package com.example.android_repo_04.view.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.android_repo_04.R
+import com.example.android_repo_04.api.GitHubApiRepository
+import com.example.android_repo_04.databinding.ActivityProfileBinding
+import com.example.android_repo_04.viewmodel.ProfileViewModel
+import com.example.android_repo_04.viewmodel.ProfileViewModelFactory
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+
+    private lateinit var viewModel: ProfileViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProvider(this,
+            ProfileViewModelFactory(GitHubApiRepository.getGitInstance()!!)
+        )[ProfileViewModel::class.java]
     }
 }
---
+++ app/src/main/java/com/example/android_repo_04/viewmodel/ProfileViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.android_repo_04.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.android_repo_04.api.GitHubApiRepository
+import com.example.android_repo_04.data.dto.profile.User
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ProfileViewModel(private val gitHubApiRepository: GitHubApiRepository): ViewModel() {
+
+    val user = MutableLiveData<User>()
+    val star = MutableLiveData<Int>()
+
+    fun requestUser(token: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            gitHubApiRepository.requestUser(token) { response ->
+                if(response.isSuccessful) {
+                    user.postValue(response.body())
+                }
+            }
+        }
+    }
+
+    fun requestUserStarred(token: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            gitHubApiRepository.requestUserStarred(token) { response ->
+                if(response.isSuccessful) {
+                    star.postValue(response.body()!!.size)
+                }
+            }
+        }
+    }
+}
+
+class ProfileViewModelFactory(private val gitHubApiRepository: GitHubApiRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(ProfileViewModel::class.java)) {
+            return ProfileViewModel(gitHubApiRepository) as T
+        }
+        throw IllegalAccessException()
+    }
+}
