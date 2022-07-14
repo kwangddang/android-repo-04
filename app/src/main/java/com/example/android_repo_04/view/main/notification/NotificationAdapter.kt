b3df03b2e39119c40bde02a48eb9303bdb3c4fd8
rosf73 <alswlzhsclq@naver.com>
2022-07-14 12:29:39 +0900

    [FEAT] [Notifications] Notifications RecyclerView Adapter 작성

--- app/src/main/java/com/example/android_repo_04/view/main/notification/NotificationAdapter.kt
+++ app/src/main/java/com/example/android_repo_04/view/main/notification/NotificationAdapter.kt
@@ -1,4 +1,34 @@
 package com.example.android_repo_04.view.main.notification
 
-class NotificationAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_repo_04.data.dto.notificaiton.Notification
+import com.example.android_repo_04.databinding.ItemNotificationBinding
+
+class NotificationAdapter: RecyclerView.Adapter<NotificationsViewHolder>() {
+
+    var notifications = listOf<Notification>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationsViewHolder {
+        return NotificationsViewHolder(
+            ItemNotificationBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: NotificationsViewHolder, position: Int) {
+        holder.bind(notifications[position])
+    }
+
+    override fun getItemCount(): Int = notifications.size
+}
+
+class NotificationsViewHolder(private val binding: ItemNotificationBinding): RecyclerView.ViewHolder(binding.root){
+    fun bind(notification: Notification){
+        binding.imgNotificationProfile
+    }
 }
