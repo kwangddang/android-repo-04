64d724fe13812258c31cc0999e376ceb218d3b78
KwangHyun Choi <camp_kwangddang12@woowafriends.com>
2022-07-14 13:38:22 +0900

    [FEAT] [Profile] 프로필 데이터 바인딩

--- app/src/main/java/com/example/android_repo_04/view/profile/ProfileActivity.kt
+++ app/src/main/java/com/example/android_repo_04/view/profile/ProfileActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.example.android_repo_04.R
 import com.example.android_repo_04.api.GitHubApiRepository
+import com.example.android_repo_04.data.db.UserToken
 import com.example.android_repo_04.databinding.ActivityProfileBinding
 import com.example.android_repo_04.viewmodel.ProfileViewModel
 import com.example.android_repo_04.viewmodel.ProfileViewModelFactory
@@ -20,11 +21,34 @@ class ProfileActivity : AppCompatActivity() {
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initViewModel()
+        initBinding()
+        requestUser()
+        requestUserStarred()
+        setOnClickListeners()
     }
 
     private fun initViewModel() {
         viewModel = ViewModelProvider(this,
             ProfileViewModelFactory(GitHubApiRepository.getGitInstance()!!)
         )[ProfileViewModel::class.java]
+    }
+
+    private fun requestUser() {
+        viewModel.requestUser("token ${UserToken.accessToken}")
+    }
+
+    private fun requestUserStarred() {
+        viewModel.requestUserStarred("token ${UserToken.accessToken}")
+    }
+
+    private fun initBinding() {
+        binding.apply {
+            vm = viewModel
+            lifecycleOwner = this@ProfileActivity
+        }
+    }
+
+    private fun setOnClickListeners() {
+        binding.imgProfileBack.setOnClickListener { onBackPressed() }
     }
 }
--- app/src/main/java/com/example/android_repo_04/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/example/android_repo_04/viewmodel/ProfileViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.android_repo_04.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
