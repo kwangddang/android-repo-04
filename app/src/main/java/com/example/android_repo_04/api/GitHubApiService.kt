d638d0be15bd2a916c05230c6e49a72486ce6553
KwangHyun Choi <camp_kwangddang12@woowafriends.com>
2022-07-14 11:07:38 +0900

    [FEAT] [Profile] 유저 api 요청 구현

--- app/src/main/java/com/example/android_repo_04/api/GitHubApiService.kt
+++ app/src/main/java/com/example/android_repo_04/api/GitHubApiService.kt
@@ -1,6 +1,8 @@
 package com.example.android_repo_04.api
 
 import com.example.android_repo_04.data.dto.issue.Issue
+import com.example.android_repo_04.data.dto.profile.Star
+import com.example.android_repo_04.data.dto.profile.User
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
@@ -13,4 +15,14 @@ interface GitHubApiService {
         @Query("state") state: String,
         @Query("filter") filter: String
     ): Response<Issue>
+
+    @GET("user")
+    suspend fun requestUser(
+        @Header("Authorization") token: String
+    ): Response<User>
+
+    @GET("user/starred")
+    suspend fun requestUserStarred(
+        @Header("Authorization") token: String
+    ): Response<List<Star>>
 }
