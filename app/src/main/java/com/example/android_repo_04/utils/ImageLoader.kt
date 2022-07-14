a50159de93888aec56f60fbee803a73a9662de96
rosf73 <alswlzhsclq@naver.com>
2022-07-14 14:23:07 +0900

    [FEAT] [Notifications] 이미지 URL to Bitmap 정적 함수 정의

--- app/src/main/java/com/example/android_repo_04/utils/ImageLoader.kt
+++ app/src/main/java/com/example/android_repo_04/utils/ImageLoader.kt
@@ -1,4 +1,39 @@
+@file: JvmName("ImageLoader")
 package com.example.android_repo_04.utils
 
-class ImageLoader {
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.IOException
+import java.net.URL
+
+val savedAvatars = mutableMapOf<String, Bitmap>()
+
+fun urlToBitmap(url: String, completed: (Bitmap?) -> Unit) {
+    if (url == "") {
+        completed(null)
+        return
+    }
+
+    if (savedAvatars.containsKey(url)) {
+        completed(savedAvatars[url])
+        return
+    }
+
+    GlobalScope.launch(Dispatchers.IO) {
+        try {
+            val bitmap = BitmapFactory.decodeStream(URL(url).openStream())
+            savedAvatars[url] = bitmap
+            withContext(Dispatchers.Main) {
+                completed(bitmap)
+            }
+        } catch (e: IOException) {
+            withContext(Dispatchers.Main) {
+                completed(null)
+            }
+        }
+    }
 }
