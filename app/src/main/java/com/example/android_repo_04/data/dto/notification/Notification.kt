e351dee9a2859d2d48bd8261b28fbab4b84f5bac
rosf73 <alswlzhsclq@naver.com>
2022-07-14 13:18:41 +0900

    [FIX] [Notifications] Notification 객체 누락된 데이터 요소 추가

--- app/src/main/java/com/example/android_repo_04/data/dto/notification/Notification.kt
+++ app/src/main/java/com/example/android_repo_04/data/dto/notification/Notification.kt
@@ -5,5 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class Notification(
     @SerializedName("id") val id: String,
     @SerializedName("repository") val repository: Repository,
+    @SerializedName("subject") val subject: Subject,
+    @SerializedName("updated_at") val updatedAt: String,
     @SerializedName("unread") val unread: Boolean
 )
