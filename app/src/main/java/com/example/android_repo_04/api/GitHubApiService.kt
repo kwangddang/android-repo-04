d49c5a48de951a99d81c1a6f1842851f3ec08457
rosf73 <alswlzhsclq@naver.com>
2022-07-14 13:21:36 +0900

    [FEAT] [Notifications] Api 통신 코드 작성

--- app/src/main/java/com/example/android_repo_04/api/GitHubApiService.kt
+++ app/src/main/java/com/example/android_repo_04/api/GitHubApiService.kt
@@ -1,7 +1,7 @@
 package com.example.android_repo_04.api
 
 import com.example.android_repo_04.data.dto.issue.Issue
-import com.example.android_repo_04.data.dto.notificaiton.Notification
+import com.example.android_repo_04.data.dto.notification.Notification
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
