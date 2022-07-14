6e86562b28d52c5407bd2488c52f7691e0fc8725
rosf73 <alswlzhsclq@naver.com>
2022-07-14 17:56:53 +0900

    [REFACTOR] issue4 - Main Fragment의 각 Data Binding 코드에 중복 코드 병합

--- app/src/main/java/com/example/android_repo_04/view/main/issue/IssueBindingAdapter.kt
+++ app/src/main/java/com/example/android_repo_04/view/main/issue/IssueBindingAdapter.kt
@@ -5,6 +5,7 @@ import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.example.android_repo_04.R
+import com.example.android_repo_04.utils.dateToFormattedString
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -27,22 +28,6 @@ object IssueBindingAdapter {
     @JvmStatic
     @BindingAdapter("issueDate")
     fun setIssueDate(view: TextView, dateString: String){
-        val fm = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX")
-        val date = fm.parse(dateString)
-        val today = Calendar.getInstance()
-        val diffMin: Long = (today.time.time - date.time) / 60000
-        val diffHour: Long = (today.time.time - date.time) / 3600000
-
-        if(diffHour < 1){
-            view.text = "${diffMin}분 전"
-        } else if(diffHour < 24){
-            view.text = "${diffHour}시간 전"
-        } else if(diffHour < 24 * 30){
-            view.text = "${diffHour / 24}일 전"
-        } else if(diffHour < 24 * 30 * 12){
-            view.text = "${diffHour / (24 * 30)}달 전"
-        } else {
-            view.text = "${diffHour / (24 * 30 * 12)}년 전"
-        }
+        view.text = dateToFormattedString(dateString)
     }
 }
--- app/src/main/java/com/example/android_repo_04/view/main/notification/NotificationBindingAdapter.kt
+++ app/src/main/java/com/example/android_repo_04/view/main/notification/NotificationBindingAdapter.kt
@@ -5,6 +5,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.example.android_repo_04.api.GitHubApiRepository
+import com.example.android_repo_04.utils.dateToFormattedString
 import com.example.android_repo_04.utils.urlToBitmap
 import kotlinx.coroutines.*
 import java.text.SimpleDateFormat
@@ -23,23 +24,7 @@ object NotificationBindingAdapter {
     @JvmStatic
     @BindingAdapter("notificationDate")
     fun setNotificationDate(view: TextView, dateString: String) {
-        val fm = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
-        val date = fm.parse(dateString)
-        val today = Calendar.getInstance()
-        val diffMin: Long = (today.time.time - date.time) / 60000
-        val diffHour: Long = (today.time.time - date.time) / 3600000
-
-        if(diffHour < 1){
-            view.text = "${diffMin}분 전"
-        } else if(diffHour < 24){
-            view.text = "${diffHour}시간 전"
-        } else if(diffHour < 24 * 30){
-            view.text = "${diffHour / 24}일 전"
-        } else if(diffHour < 24 * 30 * 12){
-            view.text = "${diffHour / (24 * 30)}달 전"
-        } else {
-            view.text = "${diffHour / (24 * 30 * 12)}년 전"
-        }
+        view.text = dateToFormattedString(dateString)
     }
 
     @JvmStatic
