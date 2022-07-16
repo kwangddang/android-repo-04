21b35f8913e875612c272af4e47a7ff03ea55e71
rosf73 <alswlzhsclq@naver.com>
2022-07-16 17:11:40 +0900

    [TEST] [Main] 화면 전환 시 저장된 프래그먼트 인스턴스를 받아와 view에 표현

--- app/src/main/java/com/example/android_repo_04/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/android_repo_04/viewmodel/MainViewModel.kt
@@ -1,4 +1,23 @@
 package com.example.android_repo_04.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class MainViewModel: ViewModel() {
+
+    var position: Int? = null
+
+    fun changePosition(pos: Int) {
+        position = pos
+    }
+}
+
+class MainViewModelFactory: ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel() as T
+        }
+        throw IllegalAccessException()
+    }
 }
