4f28c681caec1fd0391743e0c40f64b3ee753271
KwangHyun Choi <camp_kwangddang12@woowafriends.com>
2022-07-17 19:31:41 +0900

    [REFACTOR] [Issue12] CustomViewModel로 통합 #33

---
+++ app/src/main/java/com/example/android_repo_04/viewmodel/CustomViewModelFactory.kt
@@ -0,0 +1,20 @@
+package com.example.android_repo_04.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.android_repo_04.api.GitHubApiRepository
+import com.example.android_repo_04.api.GitHubLoginRepository
+
+class CustomViewModelFactory(private val repository: Any) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            LoginViewModel(repository as GitHubLoginRepository) as T
+        } else if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            MainViewModel(repository as GitHubApiRepository) as T
+        } else if (modelClass.isAssignableFrom(ProfileViewModel::class.java)) {
+            ProfileViewModel(repository as GitHubApiRepository) as T
+        } else {
+            super.create(modelClass)
+        }
+    }
+}
