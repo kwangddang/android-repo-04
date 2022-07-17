fd4d04bc88844f2aece1658e588cca47631c131d
KwangHyun Choi <camp_kwangddang12@woowafriends.com>
2022-07-17 21:16:29 +0900

    [REFACTOR] [Issue11] API 경로 BuildConfig.String으로 수정 #32

--- app/src/main/java/com/example/android_repo_04/api/GitHubApiService.kt
+++ app/src/main/java/com/example/android_repo_04/api/GitHubApiService.kt
@@ -1,5 +1,7 @@
 package com.example.android_repo_04.api
 
+import android.os.Build
+import com.example.android_repo_04.BuildConfig
 import com.example.android_repo_04.data.dto.issue.Issue
 import com.example.android_repo_04.data.dto.notification.Comment
 import com.example.android_repo_04.data.dto.notification.Notification
@@ -9,37 +11,37 @@ import retrofit2.Response
 import retrofit2.http.*
 
 interface GitHubApiService {
-    @GET("issues")
+    @GET(BuildConfig.ISSUE_URL)
     suspend fun requestIssues(
         @Header("Authorization") token: String,
         @Query("state") state: String,
         @Query("filter") filter: String
     ): Response<Issue>
 
-    @GET("user")
+    @GET(BuildConfig.USER_URL)
     suspend fun requestUser(
         @Header("Authorization") token: String
     ): Response<User>
 
-    @GET("user/starred")
+    @GET(BuildConfig.USER_STARRED_URL)
     suspend fun requestUserStarred(
         @Header("Authorization") token: String
     ): Response<List<Star>>
 
-    @GET("notifications")
+    @GET(BuildConfig.NOTIFICATIONS_URL)
     suspend fun requestNotifications(
         @Header("Accept") header: String = "application/json",
         @Header("Authorization") token: String
     ): Response<List<Notification>>
 
-    @GET("repos/{owner}/{repo}/comments")
+    @GET(BuildConfig.COMMENTS_URL)
     suspend fun requestComments(
         @Path("owner") owner: String,
         @Path("repo") repo: String,
         @Header("Accept") header: String = "application/json"
     ): Response<List<Comment>>
 
-    @PATCH("notifications/threads/{id}")
+    @PATCH(BuildConfig.NOTIFICATIONS_READ_URL)
     suspend fun requestToReadNotification(
         @Path("id") id: String,
         @Header("Accept") header: String = "application/json",
