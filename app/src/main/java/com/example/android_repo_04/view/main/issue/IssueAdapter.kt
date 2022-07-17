50aa1354ff57971f691fda6123338360e7e2efca
KwangHyun Choi <camp_kwangddang12@woowafriends.com>
2022-07-18 00:47:38 +0900

    [REFACTOR] [Issue] RecyclerView.Adapter에서 ListAdapter로 변경

--- app/src/main/java/com/example/android_repo_04/view/main/issue/IssueAdapter.kt
+++ app/src/main/java/com/example/android_repo_04/view/main/issue/IssueAdapter.kt
@@ -2,25 +2,36 @@ package com.example.android_repo_04.view.main.issue
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android_repo_04.data.dto.issue.Issue
 import com.example.android_repo_04.data.dto.issue.IssueItem
 import com.example.android_repo_04.databinding.ItemIssueBinding
 
-class IssueAdapter: RecyclerView.Adapter<IssueViewHolder>() {
-
-    var issue: Issue = Issue()
+class IssueAdapter: ListAdapter<IssueItem, IssueViewHolder>(diffUtil) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IssueViewHolder {
         return IssueViewHolder(ItemIssueBinding.inflate(LayoutInflater.from(parent.context),parent,false))
     }
 
     override fun onBindViewHolder(holder: IssueViewHolder, position: Int) {
-        holder.bind(issue[position])
+        holder.bind(getItem(position))
     }
 
-    override fun getItemCount(): Int = issue.size
+    fun replaceItem(item: Issue) {
+        submitList(item)
+    }
 
+    companion object {
+        val diffUtil = object: DiffUtil.ItemCallback<IssueItem>() {
+            override fun areContentsTheSame(oldItem: IssueItem, newItem: IssueItem) =
+                oldItem == newItem
+
+            override fun areItemsTheSame(oldItem: IssueItem, newItem: IssueItem) =
+                oldItem.id == newItem.id
+        }
+    }
 }
 
 class IssueViewHolder(private val binding: ItemIssueBinding): RecyclerView.ViewHolder(binding.root){
