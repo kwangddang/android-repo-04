4bd0b8bef532a9a008a2b7aa45e59c50c3f1bbb0
KwangHyun Choi <camp_kwangddang12@woowafriends.com>
2022-07-18 21:22:18 +0900

    [FEAT] [Search] Language Color 구현

--- app/src/main/java/com/example/android_repo_04/view/search/SearchBindingAdapter.kt
+++ app/src/main/java/com/example/android_repo_04/view/search/SearchBindingAdapter.kt
@@ -1,13 +1,19 @@
 package com.example.android_repo_04.view.search
 
+import android.content.Context
+import android.graphics.Color
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.core.widget.doAfterTextChanged
 import androidx.databinding.BindingAdapter
 import androidx.databinding.InverseBindingAdapter
 import androidx.databinding.InverseBindingListener
 import androidx.lifecycle.ViewModel
+import com.example.android_repo_04.R
 import com.example.android_repo_04.utils.urlToBitmap
+import de.hdodenhof.circleimageview.CircleImageView
 
 object SearchBindingAdapter {
 
@@ -16,6 +22,32 @@ object SearchBindingAdapter {
     fun setNotificationOwnerAvatar(view: ImageView, avatarUrl: String, viewModel: ViewModel) {
         urlToBitmap(avatarUrl, viewModel) {
             view.setImageBitmap(it)
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter("languageColor")
+    fun setLanguageColor(view: CircleImageView, language: String?) {
+        if(language != null){
+            val color = when(language) {
+                "JavaScript" -> R.color.java_script
+                "HTML" -> R.color.html
+                "Java" -> R.color.java
+                "Python" -> R.color.python
+                "Jupyter Notebook" -> R.color.jupyter_notebook
+                "C" -> R.color.c
+                "C#" -> R.color.c_sharp
+                "CSS" -> R.color.css
+                "C++" -> R.color.c_plus
+                "Dart" -> R.color.dart
+                "PHP" -> R.color.php
+                "TypeScript" -> R.color.type_script
+                "Kotlin" -> R.color.kotlin
+                else -> R.color.white
+            }
+            view.setImageResource(color)
+        } else {
+            view.setImageResource(R.color.transparent)
         }
     }
 
